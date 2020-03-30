61ada86b2bdd2efb1ed020b0fc1707e1520bdded
Jérôme LACQUEMANT <jerome.lacquemant@gmail.com>
2020-03-30 15:47:59 +0200

    Résolution du probème de Neatbeans. Je ne sais pas à quoi il est dû.

--- views/presentation.php
+++ views/presentation.php
@@ -4,32 +4,40 @@ ob_start();
 
 ?>
 <!DOCTYPE html>
-    <html>
-        <head>
-            <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-            <link type="text/css" rel="stylesheet" href="public/css/materialize.css"  media="screen,projection"/>
-            <title>Blog de Jérôme</title>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        </head>
-    
-        <body>
+<html>
+    <head>
+        <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link type="text/css" rel="stylesheet" href="public/css/materialize.css" media="screen,projection"/>
+        <title>Blog 2.0</title>
+        <meta name="viewport" content=""width="device-width, initial-scale=1.0"/>
+    </head>
+        
+    <body>
             <?php
                 include 'views/'.$topbar.'.php';
             ?>
 
-            <div class="container" class="grey darken-4>
+            <div class="container">
                 <?php
                     include 'views/'.$page.'.php';
                 ?>
             </div>
-    
-            <!--Import jQuery before materialize.js-->
-            <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-            <script type="text/javascript" src="public/js/dashboard.func.js"></script>
-            <script type="text/javascript" src="public/js/materialize.js"></script>
-            <script type="text/javascript" src="public/js/script.js"></script>  
-            <script type="text/javascript" src="public/js/post.func.js"></script>
-        </body>
-    </html>
-   
+
+
+<!--import jQuery before materialize.js-->
+    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script type="text/javascript" src="public/js/dashboard.func.js"></script>
+    <script type="text/javascript" src="public/js/materialize.js"></script>
+    <script type="text/javascript" src="public/js/script.js"></script>  
+    <script type="text/javascript" src="public/js/post.func.js"></script>
+
+    </body>
+</html> 
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
