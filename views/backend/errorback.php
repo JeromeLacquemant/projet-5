a498f292bbf6e3e663b6edf8810c218fa935bba8
Jérôme LACQUEMANT <jerome.lacquemant@gmail.com>
2020-04-10 14:50:00 +0200

    Modification de la photo de la page d'erreur administrateur, pour bien différencier les 2 pages d'erreur.

--- views/backend/errorback.php
+++ views/backend/errorback.php
@@ -1,5 +1,5 @@
-<h1>La page demandée est introuvable.</h1>
+<h1>La page demandée est introuvable cher administrateur.</h1>
 
     <div class="col s12 m6 l4">
-        <img src="public/img/error/sad_pug.jpg" class="responsive-img" alt="chien triste"/>
+        <img src="public/img/error/sad_pug_admin.jpg" class="responsive-img" alt="chien triste"/>
     </div>
