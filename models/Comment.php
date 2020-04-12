5b45f440992edfa85e955bb4d8aa39d1dea01828
Jérôme LACQUEMANT <jerome.lacquemant@gmail.com>
2020-04-12 11:42:57 +0200

    Modification de la classe Comment.php pour éviter les erreurs dues aux appels de superglobal directs.

--- models/Comment.php
+++ models/Comment.php
@@ -63,39 +63,29 @@ class Comment extends Model
 
     // Fonction permettant de supprimer un commentaire
     function delete_comment(){
-        if (isset($_GET['delete']) AND !empty($_GET['delete'])) {
-        
-        $delete = (int) $_GET['delete'];
+        $delete = filter_input(INPUT_GET, 'delete', FILTER_SANITIZE_NUMBER_INT);
         
         $req = $this->db->prepare('DELETE FROM comments WHERE id = ?');
         $req->execute(array($delete));
 
         header("Location: index.php?page=dashboard");
-        
-        exit();    
-        }
     }
 
     // Fonction permettant d'approuver un commentaire
     function approve_comment(){
-        if (empty($_GET['approve']) || !ctype_digit($_GET['approve'])) {
-            echo("Ho ! Fallait préciser le paramètre id en GET !");
-        }
-
-        $id = $_GET['approve'];
+        $id = filter_input(INPUT_GET, 'approve', FILTER_SANITIZE_NUMBER_INT);
 
         $query = $this->db->prepare('UPDATE comments SET seen=1 WHERE id=:id');
+        
         $query->execute(['id' => $id]);
 
-        header("Location: index.php?page=dashboard");
-
-        exit();
+        header("Location: /dashboard");
     }
 
     // EN COURS
     //Fonction permettant de supprimer tous les commentaires d'un article (dans le cas où on veut supprimer un article)
     function delete_article_comments(){
-        $id = $_GET['id'];
+        $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
 
         $query = $this->db->prepare('
         DELETE 
@@ -103,11 +93,9 @@ class Comment extends Model
         JOIN articles
         ON comments.article_id = article.id
         ');
+        
         $query->execute(['id' => $id]);
 
         header("Location: /liste-de-tous-les-articles");
-
-        exit();
-
     }
 }
